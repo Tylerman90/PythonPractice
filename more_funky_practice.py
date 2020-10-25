0f87538197e8967e29b39db9006c24e4e7b97ed4
Tyler James Gunter <Tyler.james.gunter@gmail.com>
2020-10-25 19:18:13 -0400

    reverse input function

--- more_funky_practice.py
+++ more_funky_practice.py
@@ -1,4 +1,4 @@
-def integer_to_reverse_binary(integer_value):
+'''def integer_to_reverse_binary(integer_value):
     string = ''
     while integer_value > 0:
         rem = integer_value % 2
@@ -11,4 +11,15 @@ def reverse_string(input_string):
     return input_string[::-1]
 
 if __name__ == '__main__':
-    print(reverse_string(integer_to_reverse_binary(int(input()))))
+    print(reverse_string(integer_to_reverse_binary(int(input()))))'''
+
+#-----------------------------------
+
+def swap_values(user_val1, user_val2):
+    return user_val2, user_val1
+
+if __name__ == '__main__':
+    a, b = swap_values(int(input()), int(input()))
+    print(a,b)
+    
+#-----------------------
